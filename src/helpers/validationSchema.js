08a0714c8f5449bfc3a2882c715b218f8620dd39
Rafael Gerônimo <rafael@geronimo.dev>
2021-10-31 10:26:23 -0300

    :construction: Added Joi package for schema and created initial schema file

---
+++ src/helpers/validationSchema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().length(6).required(),
+  avatar: Joi.string(),
+});
+
+module.exports = {
+  userSchema,
+};
