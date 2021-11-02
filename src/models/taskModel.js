e479b18f2455e9b87bee9d44ea9c333d896e9cf1
Rafael Gerônimo <rafael@geronimo.dev>
2021-11-02 18:08:00 -0300

    :bug: correct id data returned from user login

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -4,7 +4,7 @@ const createTask = async ({ title, description, initialDate, endDate, taskDone }
   const db = await connect();
   const taskCreated = await db.collection('tasks')
     .insertOne({ title, description, initialDate, endDate, taskDone, userId });
-  const task = ({ _id: taskCreated.insertedId, title, description, initialDate, endDate, taskDone, userId });
+  const task = ({ id: taskCreated.insertedId, title, description, initialDate, endDate, taskDone, userId });
   return { statusCode: 201, task };
 };
 
--- src/server.js
+++ src/server.js
@@ -7,11 +7,12 @@ const cors = require('cors');
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
 const { authMiddleware ,userRouter, loginRouter, taskRouter } = require('./routers');
 
 const corsOptions = {
-  origin: `http://localhost:${PORT}`,
+  origin: `http://localhost:${FRONTEND_PORT}`,
 };
 
 app.use(cors(corsOptions));
@@ -19,10 +20,11 @@ app.use(cors(corsOptions));
 // ---------- USER ---------- //
 app.use('/users', authMiddleware, userRouter);
 
-// ---------- USER ---------- //
+// ---------- LOGIN ---------- //
 app.use('/login', loginRouter);
 
-// ---------- USER ---------- //
+// ---------- TASKS ---------- //
 app.use('/tasks', authMiddleware, taskRouter);
+// app.use('/tasks', taskRouter); // Development environment
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
